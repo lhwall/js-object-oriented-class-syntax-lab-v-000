44267694427382f77e86e9ccf8c1f13baf3ac99d
Lucy Wall <lhwall@gmail.com>
2018-11-27 21:16:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,4 +16,12 @@ class BoardMember {
     doCharity(){
       return `I like to help people.`
     }
+
+    releasePressStatement(){
+      return "You will see great things from Scuber."
+    }
+
+    sayHi(){
+      return `Hi, my name is ${name}. I am from ${homestate}, and I was trained in ${training}.`
+    }
 }
