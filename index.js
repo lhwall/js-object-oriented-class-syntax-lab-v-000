702e71bc17c824ceafd0862ebf572de68a9b5d06
Lucy Wall <lhwall@gmail.com>
2018-11-27 21:12:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+class BoardMember {
+  constructor(name, homeState, training){
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training;
+  }
+
+    veto(){
+      returns "No, I must disagree"
+    }
+
+    approve(){
+      returns "You can do that!"
+    }
+}
